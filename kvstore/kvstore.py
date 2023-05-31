49d099a4173dda18516c68bfa654d304c8328221
Milica Ostojic <milica.ostojic@ottonova.de>
2023-05-31 23:33:27 +0200

    add Kubernetes service and some small changes

--- kvstore/kvstore.py
+++ kvstore/kvstore.py
@@ -2,16 +2,27 @@ import threading
 
 class KeyValueStore:
     def __init__(self):
-        self.store = {}
+        self.data = {}
+        self.lock = threading.Lock()
 
     def put(self, key, value):
-        with threading.Lock():
-            self.store[key] = value
+        with self.lock:
+            if not isinstance(key, str) or not isinstance(value, str):
+                raise ValueError("Key and value must be strings")
+            self.data[key] = value
 
     def get(self, key):
-        return self.store.get(key)
+        with self.lock:
+            if not isinstance(key, str):
+                raise ValueError("Key must be a string")
+            return self.data.get(key)
 
     def delete(self, key):
-        with threading.Lock():
-            if key in self.store:
-                del self.store[key]
+        with self.lock:
+            if not isinstance(key, str):
+                raise ValueError("Key must be a string")
+            if key in self.data:
+                del self.data[key]
+                return True
+            else:
+                return False
